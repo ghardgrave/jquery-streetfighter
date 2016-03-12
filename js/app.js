2010c8d0409d38d09e34d796020520faaee9b9f3
ghardgrave <ghardgrave@gmail.com>
2016-03-12 13:45:17 -0800

    Final Version

--- js/app.js
+++ js/app.js
@@ -1,43 +1,55 @@
-$(document).ready(function() {
+$(document).ready(function(){
+    $('.ryu').mouseenter(function(){
+        $('.ryu-still').hide();
+        $('.ryu-ready').show();
+    })
+    .mouseleave(function() {
+        $('.ryu-still').show();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').hide();
+        $('.ryu-cool').hide();
 
- $('.ryu').mouseenter(function() {
-    $('.ryu-still').hide();
-	$('.ryu-ready').show();
-  })
+    })
+    .mousedown(function(){
+        playHadouken();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show().animate(
+            {'left': '1020px'},
+            500,
+            function(){
+                $(this).hide();
+                $(this).css('left', '520px');
+            });
+                $('.ryu-cool').hide();
 
- 	.mouseleave(function() {
-$('.ryu-ready').hide();
-$('.ryu-still').show();
- $('.ryu-throwing').hide();
-
-  })
-
-
-	.mousedown(function() {
-	playHadouken();    
-	$('.ryu-ready').hide();
-    $('.ryu-throwing').show();
-    $('.hadouken').finish().show().animate(
-  {'left': '1020px'},
-  500,
-  function() {
-    $(this).hide();
-    $(this).css('left', '520px');
-  }
-);
-  })
-
-  .mouseup(function() {
-  $('.ryu-throwing').hide();
-  $('.ryu-ready').show();
-
-    // ryu goes back to his ready position
-  });
+    })
+    .mouseup(function(){
+    	$('.ryu-cool').hide();
+        $('.ryu-throwing').hide();
+        $('.ryu-ready').show();
+    });
 
 });
 
-function playHadouken () {
-  $('#hadouken-sound')[0].volume = 0.5;
-  $('#hadouken-sound')[0].load();
-  $('#hadouken-sound')[0].play();
+$(document).keydown(function(e){
+        if (e.which === 88) {
+        $('.ryu-cool').show();
+        $('.ryu-still').hide();
+        $('.ryu-ready').hide();
+    }
+    });
+
+$(document).keyup(function(e){
+        if (e.which === 88) {
+        $('.ryu-cool').hide();
+        $('.ryu-still').show();
+    }
+    });
+
+
+function playHadouken() {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
 }
